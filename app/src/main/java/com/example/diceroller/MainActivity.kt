749062cc346deea43d56328dbc5927252048fbc7
Jean Maxo Cyprien <jmcyprien@solutions.ht>
2024-07-28 12:09:48 -0400

    Ajout d'un champs pour recevoir le nombre defini par l'utilisateur et fonctionnalites

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,16 +15,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-
-        }
-
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = "6"
 
         val resultTextView2: TextView = findViewById(R.id.textView2)
         resultTextView2.text = "6"
+        val targetNumberEditText = findViewById<EditText>(R.id.targetNumberEditText)
+
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
     }
 
     private fun rollDice() {
@@ -35,7 +39,20 @@ class MainActivity : AppCompatActivity() {
         val resultTextView2: TextView = findViewById(R.id.textView2)
         resultTextView2.text = diceRoll2.toString()
 
-        if(diceRoll == diceRoll2){
+        val targetNumberEditText: TextView = findViewById(R.id.targetNumberEditText)
+
+        targetNumberEditText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val rollButton: Button = findViewById(R.id.button)
+                rollButton.isEnabled = !s.isNullOrEmpty()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+        })
+        val targetNumber = targetNumberEditText.text.toString().toInt()
+        val sum = diceRoll + diceRoll2
+        if(sum == targetNumber){
             val toast = Toast.makeText(this, "Pile, vous avez gagné!!!", Toast.LENGTH_SHORT)
             toast.show()
         }
