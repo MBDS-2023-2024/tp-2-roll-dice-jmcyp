3ca5e741c1b8474ff790cf8bca35637ffa5c2505
Jean Maxo Cyprien <jmcyprien@solutions.ht>
2024-07-28 10:02:53 -0400

    Ajout du de 2 et fonctionnalites pour les deux

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,17 +14,31 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             rollDice()
+
         }
 
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = "6"
+
+        val resultTextView2: TextView = findViewById(R.id.textView2)
+        resultTextView2.text = "6"
     }
 
     private fun rollDice() {
         val dice = Dice(6)
+        val dice2 = Dice(6)
         val diceRoll = dice.roll()
+        val diceRoll2 = dice2.roll()
+
         val resultTextView: TextView = findViewById(R.id.textView)
         resultTextView.text = diceRoll.toString()
+        val resultTextView2: TextView = findViewById(R.id.textView2)
+        resultTextView2.text = diceRoll2.toString()
+
+        if(diceRoll == diceRoll2){
+            val toast = Toast.makeText(this, "Pile, vous avez gagné!!!", Toast.LENGTH_SHORT)
+            toast.show()
+        }
     }
 
 
